e0832ed8669e1874a0a33eeb293530a6c0946809
westocks1 <neilkhatwani6@gmail.com>
2025-12-13 22:29:08 +0000

    Update index.py

--- index.py
+++ index.py
@@ -1,12 +1,1 @@
-import tkinter as tk
 
-def change_text():
-    button.config(text="Test")
-
-root = tk.Tk()
-root.title("Button Demo")
-
-button = tk.Button(root, text="Click me", command=change_text)
-button.pack(pady=20)
-
-root.mainloop()
